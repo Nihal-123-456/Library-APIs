cd162b57fc4018ff204e549715c8444dbc7fa26c
unknown <you@example.com>
2024-02-03 09:59:42 +0600

    string related fields removed

--- book/serializers.py
+++ book/serializers.py
@@ -2,14 +2,11 @@ from rest_framework import serializers
 from .models import *
 
 class ReviewSerializer(serializers.ModelSerializer):
-    book = serializers.StringRelatedField(many=False)
-    reviewer = serializers.StringRelatedField(many=False)
     class Meta:
         model = Review
         fields = '__all__'
 
 class BookSerializer(serializers.ModelSerializer):
-    genre = serializers.StringRelatedField(many=True)
     class Meta:
         model = Book
         fields = '__all__'
